8a1604f0700c9e07efb6df34df9ab847f5be6020
1000peach <codingandpeach@gmail.com>
2022-01-15 18:47:50 +0900

    feat: ThemeProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import "../styles/globals.css";
+import { ThemeProvider } from "@emotion/react";
 import type { AppProps } from "next/app";
 
+import theme from "../styles/theme";
+
+import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
