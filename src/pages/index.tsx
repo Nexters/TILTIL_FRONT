0ac6c80bdf05c1e6519905bd8193dbef092ec17c
denis-dev <denis.dev@kakaoenterprise.com>
2022-01-15 20:03:35 +0900

    #3 QueryClientProvider 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import type { NextPage } from "next";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+
+// export async function getStaticProps() {
+//   const queryClient = new QueryClient()
+//   await queryClient.prefetchQuery('posts', getPosts)
+
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   }
+// }
 
 const Home: NextPage = () => {
+  const { data } = useQuery("");
+
   return <div>메인</div>;
 };
 
