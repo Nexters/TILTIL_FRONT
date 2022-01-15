bef98f1396722221576a2653f55458335a9aad01
1000peach <codingandpeach@gmail.com>
2022-01-15 20:23:18 +0900

    feat: global style 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,5 +1,5 @@
-import { css } from "@emotion/react";
-import emotionReset from "emotion-reset";
+import { css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 
 const globalStyle = css`
   ${emotionReset}
@@ -20,59 +20,12 @@ const globalStyle = css`
     border: 0;
   }
 
-  body {
-    line-height: 1;
-  }
-
-  blockquote,
-  q {
-    quotes: none;
-  }
-  blockquote:before,
-  blockquote:after,
-  q:before,
-  q:after {
-    content: "";
-    content: none;
-  }
-
-  a {
-    cursor: pointer;
-  }
-
-  select {
-    cursor: pointer;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    background: transparent;
-    border: none;
-    outline: none;
-  }
-
   button {
     cursor: pointer;
     background: none;
     border: none;
     outline: none;
     padding: 0;
-  }
-
-  textarea {
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-    outline: none;
-    border: none;
-    resize: none;
-  }
-
-  textarea::-webkit-scrollbar {
-    display: none;
-  }
-
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
   }
 `;
 
