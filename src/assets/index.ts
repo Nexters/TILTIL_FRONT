f99b048be503f60e33f993882af0f78b3917f56b
1000peach <codingandpeach@gmail.com>
2022-01-31 21:52:22 +0900

    feat: header right btn icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,7 @@
 import ArrowLeft from './arrow-left.svg';
+import Cancel from './cancel.svg';
+import More from './more.svg';
+import Share from './share.svg';
+import User from './user.svg';
 
-export { ArrowLeft };
+export { ArrowLeft, Cancel, More, Share, User };
