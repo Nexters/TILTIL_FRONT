454cee5070fbe3a7305e6f20158d401073bc5025
denis-dev <denis.dev@kakaoenterprise.com>
2022-02-13 01:28:31 +0900

    feat: 로그인 페이지 추가

---
+++ src/assets/svgs/GoogleIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GoogleIcon: React.VFC<React.SVGProps<SVGSVGElement>> = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.14286 7.12245V9.57143H12.1939C12.0306 10.6224 10.9694 12.6531 8.14286 12.6531C5.70408 12.6531 3.71429 10.6327 3.71429 8.14286C3.71429 5.65306 5.70408 3.63265 8.14286 3.63265C9.53061 3.63265 10.4592 4.22449 10.9898 4.73469L12.9286 2.86735C11.6837 1.70408 10.0714 1 8.14286 1C4.19388 1 1 4.19388 1 8.14286C1 12.0918 4.19388 15.2857 8.14286 15.2857C12.2653 15.2857 15 12.3878 15 8.30612C15 7.83674 14.949 7.47959 14.8878 7.12245H8.14286Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default GoogleIcon;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,128 @@
+import { ROUTE } from 'constants/route';
+
+import styled from '@emotion/styled';
+import icebergAnimation from 'assets/lotties/iceUpDown.json';
+import GoogleIcon from 'assets/svgs/GoogleIcon';
+import Button from 'components/Button';
 import Header from 'components/layout/Header';
-import React from 'react';
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import Lottie from 'react-lottie';
 import { PageWrapper } from 'styles/styled';
 
-const Login = () => {
+const GOOGLE_LOGIN_URL = 'http://api.bing-bong.today/oauth2/authorization/google';
+
+interface Props {
+  token: string;
+}
+
+const LoginPage = ({ token }: Props) => {
+  const router = useRouter();
+  const [hasToken, setToken] = useState(false);
+
+  useEffect(() => {
+    const isStoredToken = !!localStorage.getItem('accessToken');
+    setToken(!!token || isStoredToken);
+
+    if (token) {
+      localStorage.setItem('accessToken', token);
+      router.push(ROUTE.main);
+    } else if (isStoredToken) {
+      router.push(ROUTE.main);
+    }
+  }, []);
+
+  const handleLogin = () => {
+    router.push(`${GOOGLE_LOGIN_URL}?redirect_uri=${window.location.origin}/login`);
+  };
+
   return (
-    <PageWrapper>
-      <Header rightButton={['cancel']} />
-      <main>LOGIN</main>
-    </PageWrapper>
+    !hasToken && (
+      <PageWrapper>
+        <Header rightButton={['cancel']} />
+        <DescriptionWrapper className="mx-3">
+          <span>요즘 잘나가는 사람들의</span>
+          <span>회고 방법,</span>
+          <span>BingBong</span>
+        </DescriptionWrapper>
+        <IcebergWrapper>
+          <LottieWrapper>
+            <Lottie
+              style={{ margin: '0 28px' }}
+              width={170}
+              height={246}
+              options={{
+                loop: true,
+                autoplay: true,
+                animationData: icebergAnimation,
+              }}
+            />
+          </LottieWrapper>
+        </IcebergWrapper>
+
+        <BottomSurface>
+          <ButtonWrapper>
+            <LoginButton fullWidth onClick={handleLogin}>
+              <GoogleIcon />
+              Google로 계속하기
+            </LoginButton>
+          </ButtonWrapper>
+        </BottomSurface>
+      </PageWrapper>
+    )
   );
 };
 
-export default Login;
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${({ theme }) => theme.typography.h2}
+  line-height: 38px;
+  letter-spacing: -0.04em;
+  padding: 70px 0;
+
+  span:nth-of-type(3) {
+    font-weight: bold;
+  }
+`;
+
+const LottieWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const IcebergWrapper = styled.div`
+  position: absolute;
+`;
+
+const BottomSurface = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin: 0 auto;
+  margin-top: 100px;
+  background-color: ${({ theme }) => theme.colors.primary.light};
+`;
+
+const LoginButton = styled(Button)`
+  justify-content: center;
+  align-items: center;
+  svg {
+    margin: 0 26px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  padding: 18px;
+`;
+
+export async function getServerSideProps({ query }: GetServerSidePropsContext<Pick<Props, 'token'>>) {
+  return {
+    props: {
+      token: query.token ?? '',
+    },
+  };
+}
+
+export default LoginPage;
