c28923e649d359d74eb034951c0a48d08c10e1d0
1000peach <codingandpeach@gmail.com>
2022-02-19 23:33:24 +0900

    feat: 404 Header 추가
    - Icon 컴포넌트 svg props 추가

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -27,7 +27,7 @@ export type IconName = keyof typeof ICONS;
 export type LeftButtonIconName = 'home';
 export type RightButtonIconName = 'user' | 'share' | 'more' | 'cancel';
 
-interface IconProps {
+interface IconProps extends React.SVGProps<SVGSVGElement> {
   className?: string;
   name: IconName;
 }
@@ -53,9 +53,9 @@ const ICONS = {
   info: Info,
 } as const;
 
-export const Icon: VFC<IconProps> = ({ name, className }) => {
+export const Icon: VFC<IconProps> = ({ name, className, ...rest }) => {
   // return React.createElement(ICONS[name]);
-  return <Wrap className={className}>{React.createElement(ICONS[name])}</Wrap>;
+  return <Wrap className={className}>{React.createElement(ICONS[name], rest)}</Wrap>;
 };
 
 const Wrap = styled.div`
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,28 @@
+import styled from '@emotion/styled';
+import { Icon } from 'components/icon/Icon';
+import Header from 'components/layout/Header';
 import React from 'react';
 import { PageWrapper } from 'styles/styled';
 
 const Custom404: React.VFC = () => {
-  return <PageWrapper>404 에러 페이지</PageWrapper>;
+  return (
+    <PageWrapper>
+      <Header
+        title={
+          <Title>
+            <Icon name="logo" width={86} />
+          </Title>
+        }
+      />
+      404 에러 페이지
+    </PageWrapper>
+  );
 };
+
+const Title = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+`;
 
 export default Custom404;
