1f139f76668ecc3036c992b4d72725b21cc61a39
geonwoo.jeong <doscmdev@gmail.com>
2022-02-19 19:08:37 +0900

    refactor: 컴포넌트 분리

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,14 +1,10 @@
-import styled from '@emotion/styled';
-import { Icon } from 'components/icon/Icon';
+import GuideIllust from 'components/GuideIllust';
 import Header from 'components/layout/Header';
 import MontlyLog from 'components/MontlyLog';
-import { Text } from 'components/Text';
 import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 import { PageWrapper } from 'styles/styled';
-import theme from 'styles/theme';
 import isMobileDetect from 'utils/isMobileDetect';
-import media from 'utils/media';
 
 interface Props {
   id?: string;
@@ -20,52 +16,12 @@ const Profile = ({ id, isMobile }: Props) => {
     <PageWrapper background="default">
       <Header rightButton={['share', 'user']} />
       <main>
-        <Wrapper>
-          <GuideIllust>
-            <Icon name={isMobile ? 'cloudWithMountainM' : 'cloudWithMountainD'} />
-          </GuideIllust>
-          <TextWrapper>
-            <Text typography="h2" color={theme.colors.text.highlight}>
-              최다예님
-            </Text>
-            <Text typography="h1" color={theme.colors.text.highlight}>
-              오늘 배운 것을 기록해 볼까요?
-            </Text>
-          </TextWrapper>
-        </Wrapper>
+        <GuideIllust isMobile={isMobile} />
         <MontlyLog />
       </main>
     </PageWrapper>
   );
 };
-
-const Wrapper = styled.section`
-  height: 306px;
-  background-color: ${({ theme: { colors } }) => colors.background.white};
-
-  ${media.mobile} {
-    height: 254px;
-  }
-`;
-
-const GuideIllust = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-  background: radial-gradient(43.7% 86.49% at 100% 100%, rgba(193, 225, 255, 0.3) 0%, rgba(193, 225, 255, 0) 100%),
-    radial-gradient(74.61% 74.61% at 1.25% 100%, rgba(58, 161, 255, 0.3) 0%, rgba(58, 161, 255, 0) 100%);
-  border-radius: 0px;
-  transform: matrix(1, 0, 0, -1, 0, 0);
-`;
-
-const TextWrapper = styled.div`
-  position: relative;
-  top: -93px;
-  padding: 0px 24px;
-  display: flex;
-  flex-direction: column;
-`;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const isMobile = isMobileDetect(context.req);
