56929547083044b8d5bff9804c3537170775bb33
1000peach <codingandpeach@gmail.com>
2022-02-19 22:52:32 +0900

    feat: 404 페이지 라우팅

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageWrapper } from 'styles/styled';
+
+const Custom404: React.VFC = () => {
+  return <PageWrapper>404 에러 페이지</PageWrapper>;
+};
+
+export default Custom404;
