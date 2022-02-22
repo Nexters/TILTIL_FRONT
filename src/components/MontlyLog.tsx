2ec4f34a3ab9991550528b98b3c552f782da5bff
1000peach <codingandpeach@gmail.com>
2022-02-23 03:02:23 +0900

    feat: guide modal 추가
    - 일부 svg props 설정

--- src/components/MontlyLog.tsx
+++ src/components/MontlyLog.tsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
 import { TilLogResponse } from 'apis/api';
+import { IceCubesIcon } from 'assets/svgs';
+import { ZeroCommit } from 'assets/svgs/CountIcsIcon';
 import Link from 'next/link';
 import React from 'react';
+import { useDialogStore } from 'states/dialogStore';
 import theme from 'styles/theme';
 
 import ButtonSmall from './ButtonSmall';
 import CalandarGrapth from './CalandarGrapth';
+import { Dialog } from './dialog/Dialog';
 import { Icon } from './icon/Icon';
 import { Text } from './Text';
 
@@ -18,8 +22,11 @@ interface MontlyTilsProps {
 type Props = Partial<MontlyTilsProps>;
 
 const MontlyLog = ({ total = 11, logs = dummy }: Props) => {
+  const { open } = useDialogStore();
+
   const startAt = logs[0].date;
   const endAt = logs[logs.length - 1].date;
+
   return (
     <MontlyTils className="mt-5">
       <MontlyTitle className="mb-3 mb-half">
@@ -39,7 +46,51 @@ const MontlyLog = ({ total = 11, logs = dummy }: Props) => {
       </MontlyTitle>
       <CalandarGrapth logs={logs} />
       <Note>
-        <Icon className="mr-half" name="info" />
+        <button
+          type="button"
+          onClick={() =>
+            open(
+              <Dialog
+                message=""
+                RenderContent={() => (
+                  <DialogContent>
+                    <Text typography="body3" color={theme.colors.text.highlight} fontWeight={700}>
+                      빙하 조각은 이렇게 채워져요!
+                    </Text>
+                    <CommitList>
+                      <IceCubesIcon
+                        width={180}
+                        style={{
+                          position: 'relative',
+                          transform: 'translateX(7px)',
+                        }}
+                      />
+                    </CommitList>
+                    <Text typography="caption1" color={theme.colors.icon.idle} style={{ marginBottom: 12 }}>
+                      암묵지를 쓸 때 많은 카테고리를 채울수록
+                      <br />
+                      빙하 조각 색상이 진해져요!
+                    </Text>
+                    <ZeroCommit stroke={theme.colors.ui.disabled} />
+                    <Text typography="caption1" color={theme.colors.icon.idle} className="mt-1 mb-4">
+                      쓰지 않은 날은 슬픈 표정의 빙하 조각이 추가돼요.
+                    </Text>
+                    <Text typography="body3" color={theme.colors.text.highlight} className="mb-1">
+                      날짜 표시는 어떻게 되나요?
+                    </Text>
+                    <Text typography="caption1" color={theme.colors.icon.idle}>
+                      첫 가입부터 30일까지 순차적으로 채워지고
+                      <br />
+                      30일이 초과되면 표시되는 날짜가 하루씩 밀려요.
+                    </Text>
+                  </DialogContent>
+                )}
+              />
+            )
+          }
+        >
+          <Icon className="mr-half" name="info" />
+        </button>
         <Text typography="caption1" color={theme.colors.text.placeholder}>
           {startAt} ~ {endAt}
         </Text>
@@ -71,6 +122,16 @@ const Note = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 100%;
+`;
+
+// Dialog
+const DialogContent = styled.div`
+  padding: 0 16px 24px;
+`;
+const CommitList = styled.div`
+  display: flex;
+  justify-content: center;
+  column-gap: 8px;
 `;
 
 export default MontlyLog;
