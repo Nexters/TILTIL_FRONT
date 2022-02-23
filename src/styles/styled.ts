49d1857fd3123fe9bda08a1f91e7e3ce14097020
1000peach <codingandpeach@gmail.com>
2022-02-24 00:00:25 +0900

    fix: layout 수정

--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -6,19 +6,22 @@ import globalTheme from './theme';
 
 // layout
 export const PageWrapper = styled.div<{ background?: Background }>`
+  width: 100%;
   max-width: ${({ theme: { size } }) => size.desktop}px;
+  height: max-content;
+  min-height: 100%;
+
   display: flex;
   flex-direction: column;
-  width: 100%;
-  min-height: 100vh;
-  height: 100%;
+
   background-color: ${({ theme: { colors }, background = 'white' }) => colors.background[background]};
 `;
 
 type MainProps = { padding?: keyof typeof globalTheme.padding };
 export const Main = styled.main<MainProps>`
   ${({ theme, padding }) => padding && `padding: 0 ${theme.padding[padding]}px`};
-  min-height: ${({ theme: { header } }) => `calc(100vh - ${header.desktop}px)`};
+  padding-top: ${({ theme: { header } }) => header.desktop}px;
+  flex: 1;
 `;
 
 // common
